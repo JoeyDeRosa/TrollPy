22c0275a8c06932176fe90debcb3335476f8949d
Joey DeRosa <joeyderosa11.jd@gmail.com>
2017-01-10 18:33:11 -0800

    fen format added and like a test or two

--- chess_game.py
+++ chess_game.py
@@ -12,12 +12,14 @@ def game():
     """Run the chess game."""
     while not board.is_game_over():
         print(board)
+        print(board.fen())
         move = input('your move: ')
         while chess.Move.from_uci(move) not in board.legal_moves:
             print('Not a legal move.')
             move = input('your move: ')
         board.push_uci(move)
         print(board)
+        print(board.fen())
         troll_move = troll()
         board.push(troll_move)
 
@@ -27,9 +29,7 @@ def troll():
     possible_moves = []
     for i in board.legal_moves:
         possible_moves.append(i)
-    print(possible_moves)
     troll_move = random.randint(0, len(board.legal_moves) - 1)
-    print(troll_move)
     return possible_moves[troll_move]
 
 
