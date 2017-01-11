b180943b0806e3889bdde05ecf8287aa9513bac3
Joey DeRosa <joeyderosa11.jd@gmail.com>
2017-01-11 08:48:40 -0800

    troll logic

--- chess_game.py
+++ chess_game.py
@@ -10,7 +10,7 @@ board = chess.Board()
 
 def game():
     """Run the chess game."""
-    while not board.is_game_over():
+    while True:
         print(board)
         print(board.fen())
         move = input('your move: ')
@@ -18,19 +18,52 @@ def game():
             print('Not a legal move.')
             move = input('your move: ')
         board.push_uci(move)
+        if board.is_game_over():
+            return 'You win.'
         print(board)
         print(board.fen())
         troll_move = troll()
         board.push(troll_move)
+        if board.is_game_over():
+            return 'You lose.'
 
 
 def troll():
     """Generate a move for the troll."""
-    possible_moves = []
-    for i in board.legal_moves:
-        possible_moves.append(i)
-    troll_move = random.randint(0, len(board.legal_moves) - 1)
-    return possible_moves[troll_move]
+    moves = {
+        'high_priority_moves': [],
+        'med_priority_moves': [],
+        'low_priority_moves': [],
+    }
+    moves = prioritize_troll_moves(moves)
+    prefered_move_list = select_move_list(moves)
+    troll_move = random.randint(0, len(prefered_move_list) - 1)
+    return prefered_move_list[troll_move]
+
+
+def prioritize_troll_moves(moves):
+    """Return a dictionary of the trolls moves seperated by priority."""
+    for move in board.legal_moves:
+        if board.is_capture(move):
+            if board.is_attacked_by(chess.WHITE, move.to_square):
+                moves['med_priority_moves'].append(move)
+            else:
+                moves['high_priority_moves'].append(move)
+        else:
+            if board.is_attacked_by(chess.WHITE, move.to_square):
+                moves['low_priority_moves'].append(move)
+            else:
+                moves['med_priority_moves'].append(move)
+    return moves
+
+
+def select_move_list(moves):
+    """Return the list of moves with the highest priority for the troll to choose from."""
+    if len(moves['high_priority_moves']) > 0:
+        return moves['high_priority_moves']
+    if len(moves['med_priority_moves']) > 0:
+        return moves['med_priority_moves']
+    return moves['low_priority_moves']
 
 
 if __name__ == '__main__':
