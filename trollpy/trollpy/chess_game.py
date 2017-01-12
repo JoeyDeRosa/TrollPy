1d8f6acd36195607196e61b8c088b68636534fc6
Joey DeRosa <joeyderosa11.jd@gmail.com>
2017-01-11 16:46:12 -0800

    troll talks shit

--- trollpy/trollpy/chess_game.py
+++ trollpy/trollpy/chess_game.py
@@ -3,10 +3,14 @@
 
 import chess
 import random
+from trollpy import models
+from sqlalchemy.orm import sessionmaker
 
 
 def users_game(user_board):
     """The game for the specific user."""
+    winner = None
+
     def game(user_board):
         """Run the chess game."""
         board = get_board(user_board)
@@ -18,6 +22,7 @@ def users_game(user_board):
         print(board.fen())
         troll_move = troll(board)
         board.push(troll_move)
+        talk_shit(troll_move, board)
         if board.is_game_over():
             winner = 'Troll'
         return board.fen()
@@ -61,6 +66,24 @@ def users_game(user_board):
         if len(moves['med_priority_moves']) > 0:
             return moves['med_priority_moves']
         return moves['low_priority_moves']
+
+    def talk_shit(troll_move, board):
+        piece_lvl = {
+            'K': 9,
+            'Q': 8,
+            'B': 4,
+            'N': 3,
+            'R': 2,
+            'P': 1,
+        }
+        engine = models.get_engine(registry.settings)
+        Session = sessionmaker(bind=engine)
+        session = Session()
+        shit_talk = session.query(models.KillScore).all()
+        if board.piece_at(troll_move.to_square) is None:
+            print('Baaaadum Baaadum Badum BadumBaduBadem.')
+        else:
+            lvl = piece_lvl[board.piece_at(troll_move.to_square) - piece_lvl[board.piece_at(troll_move.from_square)]
+            print(shit_talk[lvl])
     troll_move = game(user_board)
-    winner = None
     return (troll_move, winner)
