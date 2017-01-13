74ec2d800055c534286abf6c5795722cca67dc57
copenbacon <sclary50@gmail.com>
2017-01-12 16:25:25 -0800

    CSRF Protection

--- trollpy/security.py
+++ trollpy/security.py
@@ -2,8 +2,24 @@
 import os
 from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
+from pyramid.security import Allow, Everyone, Authenticated
+from pyramid.session import SignedCookieSessionFactory
 from trollpy.models import User
-from passlib.apps import  custom_app_context as pwd_context
+from passlib.apps import custom_app_context as pwd_context
+
+
+class NewRoot(object):
+    """MyRoot Object, request object."""
+
+    def __init__(self, request):
+        """Initialize the MyRoot object."""
+        self.request = request
+
+    __acl__ = [
+        (Allow, Authenticated, 'add'),
+        (Allow, Everyone, 'view'),
+    ]
+
 
 def check_credentials(request):
     if "username" in request.POST and "password" in request.POST:
@@ -14,8 +30,8 @@ def check_credentials(request):
         if the_user:
             if pwd_context.verify(password, the_user.password):
                 return True
-
     return False
+
 
 def includeme(config):
     """security-related configuration"""
@@ -27,3 +43,10 @@ def includeme(config):
     config.set_authentication_policy(authn_policy)
     authz_policy = ACLAuthorizationPolicy()
     config.set_authorization_policy(authz_policy)
+    config.set_default_permission("view")
+    config.set_root_factory(NewRoot)
+    # CSRF Protection
+    session_secret = os.environ.get("SESSION_SECRET", "itsaseekrit")
+    session_factory = SignedCookieSessionFactory(session_secret)
+    config.set_session_factory(session_factory)
+    config.set_default_csrf_options(require_csrf=True)
